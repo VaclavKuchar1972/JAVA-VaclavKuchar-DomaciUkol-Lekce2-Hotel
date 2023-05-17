4c738a0f37fa68a4aaee8dad87817800bc74e56d
VaclavKuchar1972 <132149396+VaclavKuchar1972@users.noreply.github.com>
2023-05-17 21:05:37 +0200

    Znovu a od záčátku lépe 01...

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,18 @@
 import com.domaci.ukol2.Guest;
-
 import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
-
         String hotel1Name = "MODRÁ HVĚZDA";
+        int numberGuests = 2;
 
-        Integer numberGuests = 2;
+        Guest[] HotelGuest = new Guest[numberGuests];
 
+        HotelGuest[0] = new Guest("Václav", "Kuchař", LocalDate.of(1972, 4, 1));
+        HotelGuest[1] = new Guest("Jana", "Novotná", LocalDate.of(1985, 9, 15));
 
-        Guest HotelGuest
-                = new Guest("Václav", "Kuchař", LocalDate.of(1972, 4, 1));
-
-
-
-
-        System.out.println(HotelGuest.getNameFull() + "   Datum narození: " + HotelGuest.getDateBirth());
-
-
-
-
+        System.out.println(HotelGuest[0].getNameFull() + "   Datum narození: " + HotelGuest[0].getDateBirth());
+        System.out.println(HotelGuest[1].getNameFull() + "   Datum narození: " + HotelGuest[1].getDateBirth());
     }
+
 }
+
