7364b77e323e19ff0a6506d4d7e269ac67a4b206
VaclavKuchar1972 <132149396+VaclavKuchar1972@users.noreply.github.com>
2023-05-17 19:47:36 +0200

    Znovu a od záčátku lépe 01...

--- src/com/domaci/ukol2/Guest.java
+++ src/com/domaci/ukol2/Guest.java
@@ -1,2 +1,28 @@
-package com.domaci.ukol2;public class Guest {
+package com.domaci.ukol2;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Guest {
+
+    String nameFirst;
+    String nameSecond;
+    LocalDate dateBirth;
+
+    String nameFull;
+
+    public Guest(String nameFirst, String nameSecond, LocalDate dateBirth) {
+        this.nameFirst = nameFirst;
+        this.nameSecond = nameSecond;
+        this.dateBirth = dateBirth;
+    }
+
+    public String getNameFull() {
+        return nameFirst + " " + nameSecond;
+    }
+    public String getDateBirth() {
+        return dateBirth.format(DateTimeFormatter.ofPattern("d.M.yyyy"));
+    }
+
+
 }
