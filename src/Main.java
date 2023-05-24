eec27b73e0801e3e2d03d5faf16e60a68754be0d
VaclavKuchar1972 <132149396+VaclavKuchar1972@users.noreply.github.com>
2023-05-24 18:33:35 +0200

    DOMÁCÍ ÚKOL Lekce 2 - Funkční Finále - verze LIST

--- src/Main.java
+++ src/Main.java
@@ -11,21 +11,41 @@ public class Main {
     public static void main(String[] args) {
         // ZAČÁTEK pomocných proměnných
         int i = 0;
-        int numberGuests;
+        // int numberGuests;
         int numberOfRooms;
         int numberOfBookings;
         // KONEC pomocných proměnných
 
         // ZAČÁTEK načtení proměnných, který by se podle mě měli načítat z nějaký databáze
         String hotel1Name = "MODRÁ HVĚZDA";
+
+        // Guest HotelGuest = new Guest("Adéla", "Malíková", LocalDate.of(1993, 3, 13));
+        List<Guest> listGuest = new ArrayList<>();
+        listGuest.add(new Guest("Adéla", "Malíková", LocalDate.of(1993, 3, 13)));
+        listGuest.add(new Guest("Jan", "Dvořáček", LocalDate.of(1995, 5, 5)));
+
+        System.out.println("SEZNAM HOSTŮ hotelu " + hotel1Name + ":");
+        for (Guest guest : listGuest) {
+            System.out.println("   " + guest.getNameFull());
+
+            // System.out.println(guest.getNameFull() + " " + guest.getDateBirth());
+        }
+
+        //List<Reservation> listOfReservations = new ArrayList<>();
+        //for (i = 0; i < numberOfBookings; i++) {
+        //    listOfReservations.add(new Reservation(HotelReservation[i].getWhoBooked()));
+
         // Proměnné HOST
-        numberGuests = 2;
-        Guest[] HotelGuest = new Guest[numberGuests];
-        HotelGuest[0] = new Guest("Adéla", "Malíková",
-                LocalDate.of(1993, 3, 13));
-        HotelGuest[1] = new Guest("Jan", "Dvořáček",
-                LocalDate.of(1995, 5, 5));
+        //numberGuests = 2;
+        //Guest[] HotelGuest = new Guest[numberGuests];
+        //HotelGuest[0] = new Guest("Adéla", "Malíková",
+        //        LocalDate.of(1993, 3, 13));
+        //HotelGuest[1] = new Guest("Jan", "Dvořáček",
+        //        LocalDate.of(1995, 5, 5));
         // Konec Proměnných HOST
+
+
+
         // Proměnné POKOJ
         numberOfRooms = 3;
         Room[] HotelRooms = new Room[numberOfRooms];
@@ -34,33 +54,36 @@ public class Main {
         HotelRooms[2] = new Room(3,3,false,true, 2400);
         // Konec Proměnných POKOJ
         // Proměnné REZERVACE
-        numberOfBookings = 2;
-        Reservation[] HotelReservation = new Reservation[numberOfBookings];
-        HotelReservation[0] = new Reservation(HotelGuest[0].getNameFull(),
-                LocalDate.of(2021, 7, 19),
-                LocalDate.of(2021, 7, 26), HotelRooms[0].getRooomNumber());
-        HotelReservation[1] = new Reservation(HotelGuest[0].getNameFull() + " a "
-                + HotelGuest[1].getNameFull(), LocalDate.of(2021, 9, 1),
-                LocalDate.of(2021, 9, 14), HotelRooms[2].getRooomNumber());
+        //numberOfBookings = 2;
+        //Reservation[] HotelReservation = new Reservation[numberOfBookings];
+        //HotelReservation[0] = new Reservation(HotelGuest[0].getNameFull(),
+        //        LocalDate.of(2021, 7, 19),
+        //        LocalDate.of(2021, 7, 26), HotelRooms[0].getRooomNumber());
+        //HotelReservation[1] = new Reservation(HotelGuest[0].getNameFull() + " a "
+        //        + HotelGuest[1].getNameFull(), LocalDate.of(2021, 9, 1),
+        //        LocalDate.of(2021, 9, 14), HotelRooms[2].getRooomNumber());
         // HotelRooms[1] = new Reservation(2,1,true,true, 1000);
         // Konec Proměnných REZERVACE
         // KONEC načtení proměnných, který by se podle mě měli načítat z nějaký databáze
 
         // ZAČÁTEK vytvoření rozhraní typu List pro Rezervace
-        List<Reservation> listOfReservations = new ArrayList<>();
-        for (i = 0; i < numberOfBookings; i++) {
-            listOfReservations.add(new Reservation(HotelReservation[i].getWhoBooked()));
-            listOfReservations.add(new Reservation(HotelReservation[i].getRozsahRezervace()));
-            listOfReservations.add(new Reservation(HotelReservation[i].getNumberBookedRoom()));
-        }
+        //List<Reservation> listOfReservations = new ArrayList<>();
+        //for (i = 0; i < numberOfBookings; i++) {
+        //    listOfReservations.add(new Reservation(HotelReservation[i].getWhoBooked()));
+        //    listOfReservations.add(new Reservation(HotelReservation[i].getRozsahRezervace()));
+        //    listOfReservations.add(new Reservation(HotelReservation[i].getNumberBookedRoom()));
+        //}
         // KONEC vytvoření rozhraní typu List pro Rezervace
 
         // ZAČÁTEK výstupu
-        System.out.println("SEZNAM HOSTŮ hotelu " + hotel1Name + ":");
-        for (i = 0; i < numberGuests; i++) {
-            System.out.println("   " + HotelGuest[i].getNameFull());
-            System.out.println("      Datum narození: " + HotelGuest[i].getDateBirth());
-        }
+
+        //System.out.println("SEZNAM HOSTŮ hotelu " + hotel1Name + ":");
+        //for (i = 0; i < numberGuests; i++) {
+        //    System.out.println("   " + HotelGuest[i].getNameFull());
+        //    System.out.println("      Datum narození: " + HotelGuest[i].getDateBirth());
+        //}
+
+
         System.out.println("");
         System.out.println("SEZNAM POKOJŮ hotelu " + hotel1Name + ":");
         for (i = 0; i < numberOfRooms; i++) {
@@ -71,13 +94,13 @@ public class Main {
             System.out.println("Cena za jednu noc: " + HotelRooms[i].getRoomPrice() + ",- Kč");
         }
         System.out.println("");
-        System.out.println("SEZNAM REZERVACÍ hotelu " + hotel1Name + " ke dni "
-                + LocalDate.now().format(DateTimeFormatter.ofPattern("d.M.yyyy")) +":");
-        for (i = 0; i < numberOfBookings; i++) {
-            System.out.println("     Jméno hosta/ů: " + HotelReservation[i].getWhoBooked());
-            System.out.println("  Rozsah rezervace: " + HotelReservation[i].getRozsahRezervace());
-            System.out.println("Rezervace na pokoj: " + "č." + HotelReservation[i].getNumberBookedRoom());
-        }
+        //System.out.println("SEZNAM REZERVACÍ hotelu " + hotel1Name + " ke dni "
+        //        + LocalDate.now().format(DateTimeFormatter.ofPattern("d.M.yyyy")) +":");
+        //for (i = 0; i < numberOfBookings; i++) {
+        //    System.out.println("     Jméno hosta/ů: " + HotelReservation[i].getWhoBooked());
+        //    System.out.println("  Rozsah rezervace: " + HotelReservation[i].getRozsahRezervace());
+        //    System.out.println("Rezervace na pokoj: " + "č." + HotelReservation[i].getNumberBookedRoom());
+        //}
         // KONEC výstupu
     }
 }
